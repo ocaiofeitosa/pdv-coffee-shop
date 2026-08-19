f7c466721a7c0a4a3f93be3b1a5a4aee7bebda39
Caio Feitosa <devcaioeduardo@gmail.com>
2026-08-19 01:55:28 -0300

    feat(backend): adiciona schema, service, controller e rota POST de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/multer.js';
 import { CreateUserController } from './controllers/user/CreateUserController.js';
 import { validateSchema } from './middlewares/validateSchema.js';
 import { authUserSchema, createUserSchema } from './schemas/userSchema.js';
@@ -8,9 +10,11 @@ import { isAuthenticated } from './middlewares/isAuthenticated.js';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController.js';
 import { createCategorySchema } from './schemas/categorySchema.js';
 import { isAdmin } from './middlewares/isAdmin.js';
-import { DetailCategoryController } from './controllers/category/DetailCategoryController.js';
+import { DetailCategoryController } from './controllers/category/ListCategoryController.js';
+import { CreateProductController } from './controllers/product/CreateProductController.js';
+import { createProductSchema } from './schemas/productSchema.js';
 const router = Router();
-
+const upload = multer(uploadConfig);
 // user routes
 router.post(
   '/users',
@@ -35,8 +39,16 @@ router.post(
 router.get(
   '/categories',
   isAuthenticated,
-  isAdmin,
   new DetailCategoryController().handle,
 );
 
+// product routes
+router.post(
+  '/product',
+  isAuthenticated,
+  isAdmin,
+  upload.single('file'),
+  validateSchema(createProductSchema),
+  new CreateProductController().handle,
+);
 export { router };
---
+++ src/schemas/productSchema.ts
@@ -0,0 +1,16 @@
+import z from 'zod';
+
+export const createProductSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ message: 'O nome do produto precisa ser um texto' })
+      .min(1, { message: 'O nome do produto é obrigatório!' }),
+    price: z.coerce
+      .number()
+      .min(1, { message: 'O preço do produto é obrigatório' }),
+    description: z
+      .string()
+      .min(1, { message: 'A descrição do produto é obrigatória!' }),
+    category_id: z.string({ message: 'A categoria do produto é obrigatória!' }),
+  }),
+});
