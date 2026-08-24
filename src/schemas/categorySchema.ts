2344cf4a5ba7f04c6b266bdd6b4ce455ce871847
Caio Feitosa <devcaioeduardo@gmail.com>
2026-08-23 23:15:00 -0300

    fix: remove arquivos duplicados da raiz do monorepo

--- src/schemas/categorySchema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const createCategorySchema = z.object({
-  body: z.object({
-    name: z
-      .string({ message: 'O nome da categoria precisa ser um texto' })
-      .min(3, { message: 'O nome precisa possuir pelo menos 3 caracteres' }),
-  }),
-});
