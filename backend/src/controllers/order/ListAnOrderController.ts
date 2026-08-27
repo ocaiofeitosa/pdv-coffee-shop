0e0f3a88754614ede4e683f538e356c184e900e2
Caio Feitosa <devcaioeduardo@gmail.com>
2026-08-27 00:31:32 -0300

    ci: adiciona pipeline de deploy e atualiza controller

--- backend/src/controllers/order/ListAnOrderController.ts
+++ backend/src/controllers/order/ListAnOrderController.ts
@@ -4,12 +4,12 @@ import { ListAndOrderService } from '../../services/order/ListAnOrderService.js'
 class ListAndOrderController {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
-      const { order_id } = req.body;
+      const { order_id } = req.params;
       if (!order_id) {
         throw new Error('O número do pedido é obrigatório!');
       }
       const order = new ListAndOrderService();
-      const listOrder = await order.execute({ order_id });
+      const listOrder = await order.execute({ order_id: order_id as string });
       res.status(200).json(listOrder);
     } catch (error) {
       next(error);
